5d5753a999047f0f0e396854d3f204d1fdb22a54
Diego Marcolino <marcolinodiego.stn@gmail.com>
2026-05-04 17:27:02 -0300

    Corrige gitignore e adiciona AppDbContext

---
+++ DuelRecords.Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DuelRecords.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuelRecords.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
