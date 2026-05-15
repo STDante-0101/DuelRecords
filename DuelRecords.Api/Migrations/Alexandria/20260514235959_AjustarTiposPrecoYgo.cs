f63d7883210691ae45346d43ee8a87b86b26fcc1
Diego Marcolino <marcolinodiego.stn@gmail.com>
2026-05-14 21:00:37 -0300

    fix: ajusta tipos de preco da YGO

---
+++ DuelRecords.Api/Migrations/Alexandria/20260514235959_AjustarTiposPrecoYgo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DuelRecords.Api.Migrations.Alexandria
+{
+    /// <inheritdoc />
+    public partial class AjustarTiposPrecoYgo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SetPrice",
+                table: "YgoCardSets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SetPrice",
+                table: "YgoCardSets",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
